801e8cfaabb67f82efe8b374c06ede3bf4b793ae
eamre <emrealkan.1@outlook.com>
2024-07-17 23:08:17 +0300

    Logging pipeline and exception logging

---
+++ Core.Application/Pipelines/Logging/LoggingBehavior.cs
@@ -0,0 +1,43 @@
+﻿using Core.CrossCuttingConcerns.Logging;
+using Core.CrossCuttingConcerns.Serilog;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Core.Application.Pipelines.Logging;
+
+public class LoggingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>, ILoggableRequest
+{
+    private readonly IHttpContextAccessor _contextAccessor; //Hangi kullanıcı?
+    private readonly LoggerServiceBase _loggerServiceBase;
+
+    public LoggingBehavior(IHttpContextAccessor contextAccessor, LoggerServiceBase loggerServiceBase)
+    {
+        _contextAccessor = contextAccessor;
+        _loggerServiceBase = loggerServiceBase;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        List<LogParameter> logParameters =
+        [
+            new LogParameter{Type = request.GetType().Name, Value = request},
+        ];
+
+        LogDetail logDetail = new()
+        {
+            MethodName = next.Method.Name,
+            Parameters = logParameters,
+            User = _contextAccessor.HttpContext.User.Identity?.Name ?? "Unknow",           
+        };
+
+        _loggerServiceBase.Info(JsonSerializer.Serialize(logDetail));
+        return await next();
+    }
+}
--- Core.CrossCuttingConcerns/Exceptions/ExceptionMiddleware.cs
+++ Core.CrossCuttingConcerns/Exceptions/ExceptionMiddleware.cs
@@ -1,9 +1,12 @@
 ﻿using Core.CrossCuttingConcerns.Exceptions.Handlers;
+using Core.CrossCuttingConcerns.Logging;
+using Core.CrossCuttingConcerns.Serilog;
 using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Core.CrossCuttingConcerns.Exceptions;
@@ -12,11 +15,15 @@ public class ExceptionMiddleware
 {
     private readonly RequestDelegate _next;
     private HttpExceptionHandler _httpExceptionHandler;
+    private readonly IHttpContextAccessor _contextAccessor;
+    private readonly LoggerServiceBase _loggerServiceBase;
 
-    public ExceptionMiddleware(RequestDelegate next)
+    public ExceptionMiddleware(RequestDelegate next, IHttpContextAccessor contextAccessor, LoggerServiceBase loggerServiceBase)
     {
         _next = next;
         _httpExceptionHandler = new HttpExceptionHandler();
+        _contextAccessor = contextAccessor;
+        _loggerServiceBase = loggerServiceBase;
     }
 
     public async Task Invoke(HttpContext context)
@@ -27,8 +34,29 @@ public class ExceptionMiddleware
         }
         catch (Exception exception)
         {
+            LogException(context, exception);
             await HandleExceptionAsync(context.Response, exception);
         }
+    }
+
+    private Task LogException(HttpContext context, Exception exception)
+    {
+        List<LogParameter> logParameters =
+        [
+            new LogParameter() {Type = context.GetType().Name, Value = exception.ToString()}
+        ];
+
+        LogDetailWithException logDetail = new()
+        {
+            MethodName = _next.Method.Name,
+            Parameters = logParameters,
+            User = _contextAccessor.HttpContext?.User.Identity?.Name ?? "Unknow",
+            ExceptionMessage = exception.Message
+        };
+
+        _loggerServiceBase.Error(JsonSerializer.Serialize(logDetail));
+
+        return Task.CompletedTask;
     }
 
     private Task HandleExceptionAsync(HttpResponse response, Exception exception) 
